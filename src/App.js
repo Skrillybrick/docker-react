d348228d9d94b1a012a3231903e3e8f9942f5b06
Chase Smith <casmith@solutionreach.com>
2020-03-03 11:19:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload, unless you're a pussy.
+        </p>
+        <p>
+          ... yeah, you're a total puss
         </p>
         <a
           className="App-link"
